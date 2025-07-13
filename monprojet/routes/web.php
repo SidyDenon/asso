49af6bd8aeac662e321e348faf521690f4c05dc1
SidyDenon <sidydenon63@icloud.com>
2025-07-13 05:42:42 +0100

    Initial commit for Render

--- monprojet/routes/web.php
+++ monprojet/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/migrate', function () {
+    Artisan::call('migrate', ["--force" => true]);
+    return "✅ Migrations exécutées avec succès !";
+});
