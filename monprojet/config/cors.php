4d675b2c85a2eb6266fe74ec48b893824cc50282
SidyDenon <sidydenon63@icloud.com>
2025-07-13 07:35:13 +0100

    update CORS for Netlify

--- monprojet/config/cors.php
+++ monprojet/config/cors.php
@@ -2,24 +2,14 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => [
+        'https://tangerine-concha-db12b9.netlify.app',
+        'http://localhost:4200'
+    ],
 
     'allowed_origins_patterns' => [],
 
